78001b50887d0d762d8a38434632cd0ab846ac28
A.Tanoumandian <tanoomandian204@gmail.com>
2023-06-21 13:23:40 +0330

    feat: config validation for file_store of archive1 added

--- internal/archive/config.go
+++ internal/archive/config.go
@@ -3,6 +3,7 @@ package archive
 import (
 	"encoding/json"
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/ARTM2000/archive1/internal/validate"
@@ -23,11 +24,48 @@ type Auth struct {
 	JWTExpireTime time.Duration `mapstructure:"jwt_expire_time" json:"jwt_expire_time" validate:"required"`
 }
 
+type FileStore struct {
+	Mode       string         `mapstructure:"mode" json:"mode" validate:"required,eq=disk|eq=minio"`
+	DiskConfig *DiskFileStore `mapstructure:"disk_config" json:"disk_config" validate:"omitempty,required,dive"`
+}
+
+func (fs *FileStore) Validate() error {
+	switch fs.Mode {
+	case "disk":
+		if fs.DiskConfig == nil {
+			return fmt.Errorf("for disk mode, disk_config is required")
+		}
+		pathD, err := os.Stat(fs.DiskConfig.Path)
+		if err != nil {
+			if os.IsNotExist(err) {
+				return fmt.Errorf("path '%s' not exists", fs.DiskConfig.Path)
+			}
+			if os.IsPermission(err) {
+				return fmt.Errorf("access denied to access path '%s'", fs.DiskConfig.Path)
+			}
+			if !pathD.IsDir() {
+				return fmt.Errorf("path is not valid directory")
+			}
+		}
+	case "minio":
+		// todo: add support for minio
+		return fmt.Errorf("minio is not supported yet")
+	default:
+		return fmt.Errorf("file store mode not defined. mode: '%s'", fs.Mode)
+	}
+	return nil
+}
+
+type DiskFileStore struct {
+	Path string `mapstructure:"path" json:"path" validate:"required,filepath"`
+}
+
 type Config struct {
-	ServerPort *int     `mapstructure:"server_port" json:"server_port" validate:"omitempty,number"`
-	ServerHost *string  `mapstructure:"server_host" json:"server_host" validate:"omitempty,hostname|ip"`
-	Database   Database `mapstructure:"database" json:"database" validate:"required,dive"`
-	Auth       Auth     `mapstructure:"auth" json:"auth" validate:"required,dive"`
+	ServerPort *int      `mapstructure:"server_port" json:"server_port" validate:"omitempty,number"`
+	ServerHost *string   `mapstructure:"server_host" json:"server_host" validate:"omitempty,hostname|ip"`
+	Database   Database  `mapstructure:"database" json:"database" validate:"required,dive"`
+	Auth       Auth      `mapstructure:"auth" json:"auth" validate:"required,dive"`
+	FileStore  FileStore `mapstructure:"file_store" json:"file_store" validate:"required,dive"`
 }
 
 func (c *Config) String() string {
@@ -39,6 +77,11 @@ func (c *Config) Validate() error {
 	errors, ok := validate.ValidateStruct[Config](c)
 	if !ok {
 		return fmt.Errorf("configuration validation error: %s", errors[0].Message)
+	}
+
+	fileStoreErr := c.FileStore.Validate()
+	if fileStoreErr != nil {
+		return fmt.Errorf("file store config got error. %s", fileStoreErr.Error())
 	}
 
 	return nil
