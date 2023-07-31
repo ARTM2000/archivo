fcb5eb8c11723d62c3442fff8e8d578e2750a675
A.Tanoumandian <tanoomandian204@gmail.com>
2023-07-31 15:22:55 +0330

    chore: saving credential to local-storage added

--- web/src/auth-provider.ts
+++ web/src/auth-provider.ts
@@ -2,26 +2,22 @@ import { AuthProvider as IAuthProvider } from 'ra-core';
 import { HttpAgent } from './utils/http-agent';
 import { ArchiveResponse } from './utils/types';
 
-export const TOKEN_KEY = 'tkn';
-
 export const AuthProvider: IAuthProvider = {
   login: async (params: { email: string; password: string }) => {
     try {
-      const res = await HttpAgent.post<
-        ArchiveResponse<{ access_token: string }>
-      >('/auth/login', {
-        email: params.email,
-        password: params.password,
-      });
-      const token = res.data.data.access_token;
-      localStorage.setItem(TOKEN_KEY, token);
+      await HttpAgent.post<ArchiveResponse<{ access_token: string }>>(
+        '/auth/login',
+        {
+          email: params.email,
+          password: params.password,
+        },
+      );
     } catch (err) {
       console.log('login error', err);
       throw err;
     }
   },
   checkAuth: async (_) => {
-    const token = localStorage.getItem(TOKEN_KEY) || '';
     try {
       await HttpAgent.get<
         ArchiveResponse<{
@@ -34,9 +30,7 @@ export const AuthProvider: IAuthProvider = {
             updated_at: string;
           };
         }>
-      >('/auth/me', {
-        headers: { authorization: `Bearer ${token}` },
-      });
+      >('/auth/me');
     } catch (err) {
       console.log('check auth error: ', err);
       throw err;
@@ -45,18 +39,15 @@ export const AuthProvider: IAuthProvider = {
   checkError: async (error) => {
     const status = error.status;
     if (status === 401 || status === 403) {
-      localStorage.removeItem(TOKEN_KEY);
       return Promise.reject();
     }
-    // other error code (404, 500, etc): no need to log out
     return Promise.resolve();
   },
   logout: async () => {
-    localStorage.removeItem(TOKEN_KEY);
+    // todo: call logout route
     console.log('user logged out');
   },
   getIdentity: async () => {
-    const token = localStorage.getItem(TOKEN_KEY) || '';
     try {
       const res = await HttpAgent.get<
         ArchiveResponse<{
@@ -69,14 +60,11 @@ export const AuthProvider: IAuthProvider = {
             updated_at: string;
           };
         }>
-      >('/auth/me', {
-        headers: { authorization: `Bearer ${token}` },
-      });
+      >('/auth/me');
       console.log('user id', res.data.data.user.id);
       return {
         id: 0,
         fullName: res.data.data.user.username,
-        // avatar: ''
       };
     } catch (err) {
       console.log('check auth error: ', err);
